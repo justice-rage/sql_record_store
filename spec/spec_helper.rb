395e801cb8ecebf555b287fc8beb720d39835429
justicepelteir <justice.douglas@hey.com>
2022-05-10 20:23:42 -0700

    connect test db and clear db between spec runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,12 @@ require 'pg'
 require 'album'
 require 'song'
 require 'pry'
+
+DB = PG.connect({:dbname => 'record_store_test'})
+
+RSpec.configure do |config|
+    config.after(:each) do
+        DB.exec("DELETE FROM albums *;")
+        DB.exec("DELETE FROM songs *;")
+    end
+end
