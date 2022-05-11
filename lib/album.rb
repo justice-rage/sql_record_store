bd735799460587bf6c1a33b6de1729d9dfe49e22
justicepelteir <justice.douglas@hey.com>
2022-05-10 20:46:31 -0700

    update .all() method

--- lib/album.rb
+++ lib/album.rb
@@ -8,7 +8,14 @@ class Album
   end
 
   def self.all
-    @@albums.values()
+    returned_albums = DB.exec("SELECT * FROM albums;")
+    albums = []
+    returned_albums.each() do |album|
+      name = album.fetch("name")
+      id = album.fetch("id").to_i
+      albums.push(Album.new({:name => name, :id => id}))
+    end
+    albums
   end
 
   def save
