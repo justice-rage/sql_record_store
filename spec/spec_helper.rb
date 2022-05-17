3afedd83adfc9399069e98414af3bca00c60057f
justicepelteir <justice.douglas@hey.com>
2022-05-16 20:02:04 -0700

    update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,19 @@ require 'rspec'
 require 'pg'
 require 'album'
 require 'song'
+require 'artist'
 require 'pry'
+
+# shared code for clearing tests between runs & connecting to the DB:
 
 DB = PG.connect({:dbname => 'record_store_test'})
 
 RSpec.configure do |config|
-    config.after(:each) do
-        DB.exec("DELETE FROM albums *;")
-        DB.exec("DELETE FROM songs *;")
-    end
+  config.after(:each) do
+    DB.exec("DELETE FROM albums *;")
+    DB.exec("DELETE FROM songs *;")
+    DB.exec("DELETE FROM artists *;")
+  end
 end
+© 2022 GitHub, Inc.
+Terms
